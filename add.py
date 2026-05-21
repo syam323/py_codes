786e1317e36548f4fd9fb65e7edd5210d0280eab
Syam <syam3256@gmail.com>
2026-05-21 12:32:55 +0530

    first commit

---
+++ add.py
@@ -0,0 +1,8 @@
+Python 3.14.5 (tags/v3.14.5:5607950, May 10 2026, 10:43:50) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print(4+4)
+8
+>>> print(4-4)
+0
+>>> print(4*4)
+16
